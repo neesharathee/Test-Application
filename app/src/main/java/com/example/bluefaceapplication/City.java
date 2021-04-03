15a1fedaa06a8b1fe9de1a5507090b581c9297a4
Neesha Rathee <nisharathee203@gmail.com>
2021-04-03 01:59:53 +0100

    parse json data and log against TAG

--- app/src/main/java/com/example/bluefaceapplication/City.java
+++ app/src/main/java/com/example/bluefaceapplication/City.java
@@ -6,4 +6,32 @@ public class City {
     public String name;
     public int rank;
     public float temperature;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+    @Override
+    public String toString(){
+        return "City is: "+name+ " Rank is: " +rank;
+    }
 }
--- app/src/main/java/com/example/bluefaceapplication/MainActivity.java
+++ app/src/main/java/com/example/bluefaceapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.bluefaceapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JsonData js = new JsonData();
+        City city = new City();
+        String str = js.getJsonString();
+        try {
+            JSONObject jObj = new JSONObject(str);
+            JSONObject obj = jObj.getJSONObject("city");
+            city.setName(obj.getString("name"));
+            city.setRank(obj.getInt("rank"));
+            Log.d(TAG, city.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
